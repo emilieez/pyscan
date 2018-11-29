905a844d698ef5c1408428a507e15ca1532ff498
jsalisi <jsalisi@gmail.com>
2018-11-29 13:37:35 -0800

    added another ml algo
    Signed-off-by: jsalisi <jsalisi@gmail.com>

--- model/ModelTrainer.py
+++ model/ModelTrainer.py
@@ -1,5 +1,6 @@
 from ModelClassifier import ModelClassifier
 from sklearn import preprocessing, svm
+from sklearn.neighbors import KNeighborsClassifier
 import _pickle as p
 import os
 import pandas as pd
@@ -10,7 +11,7 @@ Currently inaccurate compared to current histogram classifier algorithm
 Machine Learning classifier requires more data to be accurate
 """
 
-def train():
+def train_svc():
     temp_list = []
     names = []
 
@@ -32,15 +33,37 @@ def train():
         p.dump(clf, training_data)
 
 
+def train_neighbors():
+    temp_list = []
+    names = []
+
+    with open("object_data.csv", 'r') as csv_data:
+        file_data = pd.read_csv(csv_data, header=None)
+        new_data = list(file_data.values)
+
+        for i in new_data:
+            extracted_data = i[1].split(',')
+            distance_data = preprocessing.scale([float(j.strip()) for j in extracted_data])
+            temp_list.append(distance_data)
+            names.append(i[0])
+            print(i[0], "added")
+
+    print("Training...")
+    with open("trained_data.pkl", 'wb') as training_data:
+        n = KNeighborsClassifier(n_neighbors=len(temp_list), algorithm="kd_tree", weights='distance')
+        n.fit(temp_list, names)
+        p.dump(n, training_data)
+
+
 def prediction():
     mesh = ModelClassifier(os.path.join(os.path.join(os.path.dirname(__file__), "scans"), "Hand.obj"))
     hist_data = preprocessing.scale(mesh.generate_distribution_data(mesh.mesh_object.vertices))
 
     with open("trained_data.pkl", "rb") as dump_file:
-        clf = p.load(dump_file)
-        print("It is a {}!".format(clf.predict([hist_data])[0]))
+        classifier = p.load(dump_file)
+        print("It is a {}!".format(classifier.predict([hist_data])[0]))
 
 
 if __name__ == '__main__':
-    #train()
+    #train_neighbors()
     prediction()
